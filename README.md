0044f73e81367b7e12ee4c3f7aab5cc8dc68faf1
yaodehuang <sef1548@gmail.com>
2016-04-25 23:03:10 +0800

    回傳交易資料的功能壞掉

--- README.md
+++ README.md
@@ -5,7 +5,12 @@
 用到的工具： nodejs,express,ngrok 
 
 
+backend中的app.js
 
+已經有送出訂單資料的功能
+只是會在前端的a tag 中的 href裡面再加上allpay的官方網址,
+
+接下來接收交易成功的功能壞掉了
 
 
 
--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var request = require('request');
-
 app = express();
+
+
+// 這個number變數是訂單變數
+var number = "adsf23fadse23we234";
+var testurl = "http://62647842.ngrok.io/e"
 
 // 產生檢查碼的表單
 var formData = {
   MerchantID: "2000132",
-  MerchantTradeNo: "12rfadf2qfasfd",
+  MerchantTradeNo: number,
+  // 每次在執行這個程式時都要更改MerchantTradeNo
   MerchantTradeDate: "2016/04/21 16:18:02",
   PaymentType: "aio",
   TotalAmount: "1",
   TradeDesc: "1",
   ItemName: "1",
-  ReturnURL: "http://73a638ba.ngrok.io/e",
+  ReturnURL: testurl,
   ChoosePayment: "Credit"
 };
 
@@ -22,6 +27,7 @@ request.post({
   formData: formData
 }, function optionalCallback(err, httpResponse, body) {
   // body 這個變數是檢查碼
+
   if (err) {
     return console.error('upload failed:', err);
   }
@@ -29,38 +35,45 @@ request.post({
   // 產出訂單的表單
   var formData2 = {
     MerchantID: "2000132",
-    MerchantTradeNo: "12rfadf2qfasfd",
+    MerchantTradeNo: number,
+    // 每次在執行這個程式時都要更改MerchantTradeNo
     MerchantTradeDate: "2016/04/21 16:18:02",
     PaymentType: "aio",
     TotalAmount: "1",
     TradeDesc: "1",
     ItemName: "1",
-    ReturnURL: "http://73a638ba.ngrok.io/e",
+    ReturnURL: testurl,
     ChoosePayment: "Credit",
     CheckMacValue: body
   };
 
-  request.post({
-    url: 'https://payment-stage.allpay.com.tw/Cashier/AioCheckOut/V2',
-    formData: formData2
-  }, function optionalCallback(err, httpResponse, body2) {
-    if (err) {
-      return console.error('upload failed:', err);
-    }
-    // console.log(httpResponse);
+  app.get('/', function(req, res) {
+    request.post({
+      url: 'https://payment-stage.allpay.com.tw/Cashier/AioCheckOut/V2',
+      formData: formData2
+    }, function optionalCallback(err, httpResponse, body2) {
+      if (err) {
+        return console.error('upload failed:', err);
+      }
+      // console.log(httpResponse);
 
-    console.log( body2);
+      var text = "https://payment-stage.allpay.com.tw/Cashier/AioCheckOut/V2";
+      res.send(body2);
+
+    });
+
   });
+
 
 
 
 });
 
 //
-
-
-
-
+app.post('/e', function(req, res) {
+  console.log(req.body);
+  // debugger;
+});
 
 
 
